6e21196cd5bfa125e6d4ddde6f2ab6ba1d86da50
LucasFigueiredoLuvas <lucas.fig50@gmail.com>
2023-10-12 16:06:05 -0300

    ADD: package 'helmet' and use in server

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -2,9 +2,11 @@ import express from "express";
 import path from "node:path";
 import { router } from "./routes/index";
 import pino from "pino-http";
+import helmet from "helmet";
 const server = express();
 const logger = pino();
 
+server.use(helmet());
 server.use(router);
 server.use(logger);
 server.set("views", path.join(__dirname, "../views"));
