78da66c242d448d8a08fc061547220f8b659b99d
LucasFigueiredoLuvas <lucas.fig50@gmail.com>
2023-10-12 15:00:55 -0300

    ADD: set 'EJS' template engine for project

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,12 +1,15 @@
 import express from "express";
+import path from "node:path";
 import { router } from "./routes/index";
 import pino from "pino-http";
 const server = express();
 const logger = pino();
 
-// server.use(express.json());
 server.use(router);
 server.use(logger);
+server.set("views", path.join(__dirname, "../views"));
+server.set("view engine", "ejs");
+server.use(express.static("public"));
 
 export {
   server
