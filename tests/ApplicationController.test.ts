b5ed5aa006bff2f153803127f76521fe3e3ff3a1
LucasFigueiredoLuvas <lucas.fig50@gmail.com>
2023-10-12 15:03:28 -0300

    ADD: first test for views (snapshot)

--- tests/ApplicationController.test.ts
+++ tests/ApplicationController.test.ts
@@ -5,6 +5,7 @@ describe("ApplicationController requests and responses", () => {
   it("Should return status-code 200", async () => {
     const response = await TestServer.get("/");
     expect(response.status).toBe(200);
+    expect(response.text).toMatchSnapshot();
   });
 
 });
