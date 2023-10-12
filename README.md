cd6ec3a5c9302e0c1656c5fb8cfd5b5d20e5337c
LucasFigueiredoLuvas <lucas.fig50@gmail.com>
2023-10-12 12:31:03 -0300

    Initial config. files for Jest, TypesCript -ADD: packages for: TS, JEST, ESLINT, EJS, EXPRESS, PINO & DOTENV

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # encurtador_de_links
+
+----------
+## Descrição:
+
+Aplicação que transforma links (geralmente longas)
+em pequenos links que facilitam o compartilhamento
+e permitem gerar informações, como quantidade de
+acessos.
+
+## Tecnologias Utilizadas:
+*em andamento...*
---
+++ jest.config.ts
@@ -0,0 +1,55 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // An array of regexp pattern strings used to skip coverage collection
+  coveragePathIgnorePatterns: [
+    "/node_modules/"
+  ],
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // A list of reporter names that Jest uses when writing coverage reports
+  coverageReporters: [
+    "json"
+  ],
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: [
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node"
+  ],
+
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  setupFilesAfterEnv: [
+    "./tests/jest.setup.ts"
+  ],
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: [
+    "<rootDir>/tests/**/*.test.ts"
+  ],
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  }
+};
