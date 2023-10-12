4392e70b33d5d4976e54256354584dcf30ac41ca
LucasFigueiredoLuvas <lucas.fig50@gmail.com>
2023-10-12 12:34:05 -0300

    ADD: First test for homepage request

---
+++ tests/ApplicationController.test.ts
@@ -0,0 +1,10 @@
+import { TestServer } from "./jest.setup";
+
+describe("ApplicationController requests and responses", () => {
+
+  it("Should return status-code 200", async () => {
+    const response = await TestServer.get("/");
+    expect(response.status).toBe(200);
+  });
+
+});
