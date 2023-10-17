462dfb3dced8e3b21a76ab6d43d38266f9f97d8c
LucasFigueiredoLuvas <lucas.fig50@gmail.com>
2023-10-17 09:43:12 -0300

    REFACTOR: just modified an message for tests

--- tests/ApplicationController.test.ts
+++ tests/ApplicationController.test.ts
@@ -2,7 +2,7 @@ import { TestServer } from "./jest.setup";
 
 describe("ApplicationController requests and responses", () => {
 
-  it("Should return status-code 200", async () => {
+  it("root path should return status-code 200", async () => {
     const response = await TestServer.get("/");
     expect(response.status).toBe(200);
     expect(response.text).toMatchSnapshot();
