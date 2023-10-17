0a3e727cc765105b74368c51a078bbb740c4344d
LucasFigueiredoLuvas <lucas.fig50@gmail.com>
2023-10-17 09:44:02 -0300

    ADD: configuration files for prisma database tests

--- src/controllers/ApplicationController/about.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export const about = (req: Request, res: Response) => {
-  return res.status(200).render("./layouts/about", { about: "Sobre" });
-};
--- src/controllers/ApplicationController/index.ts
+++
@@ -1,7 +0,0 @@
-import { about } from "./about";
-import { home } from "./home";
-
-export const ApplicationController = {
-  about,
-  home
-};
